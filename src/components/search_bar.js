55d6a4f11f748a5861208445d64d920a8b2ac409
Niccolo Ortega <nfcortega89@gmail.com>
2018-02-19 14:58:44 -0800

    added component files

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+  }
+  render() {
+    return (
+      <div>
+        <input onChange={e => this.setState({ term: e.target.value })} />
+      </div>
+    );
+  }
+}
