f778949705453ea263183c82872f611bbbe1ab2f
Niccolo Ortega <nfcortega89@gmail.com>
2018-02-19 14:58:29 -0800

    intro to state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAypiJ6eVwaxjlLfcDdVjFX7JIj-H-zkkg';
+
 // Create a new component. This component should produce some HTML.
-
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
-// Component needs to be inserted into the DOM.
 
+// Component needs to be inserted into the DOM.
 ReactDOM.render(<App />, document.querySelector('.container'));
